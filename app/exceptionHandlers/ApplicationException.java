ffddcdc8dedbc7221a5d5f9d41851275589af5fe
J Stride <horsleyParish@jonstride.uk>
2016-11-29 21:29:57 +0000

    Document uploader API service

---
+++ app/exceptionHandlers/ApplicationException.java
@@ -0,0 +1,12 @@
+package exceptionHandlers;
+
+/**
+ * Created by jstride on 29/11/2016.
+ */
+public class ApplicationException extends Exception {
+
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+}
