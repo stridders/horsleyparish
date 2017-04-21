2a28dd60731b50482735c077df3ab5aa9ec8163f
Jon Stride <js@tip.uk.com>
2017-04-21 13:30:25 +0100

    Add site map and fix json-ld issues

---
+++ app/views/web-page.js
@@ -0,0 +1,12 @@
+
+    {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "url": "http://horsleyparish.co.uk",
+        "contactPoint": [{
+            "@type": "ContactPoint",
+            "email": "websupport@jonstride.uk",
+            "contactType": "Web support"
+        }]
+    }
+
